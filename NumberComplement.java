66872da52695015f5c1c3673a2bb38f3fdeae4bd
LiJinglin918 <lijinglin918@gmail.com>
2016-11-19 01:50:15 +0700

    Update NumberComplement.java

--- NumberComplement.java
+++ NumberComplement.java
@@ -2,14 +2,21 @@
 Number Complement。给一个数，输出它的有效数位inverse之后的数（输入输出均为十进制）。
 比如5（101）的inverse是2（010）。因为牵扯到位运算，花了点时间。建议先复习一下位运算（与、或、非、异或）
 */
+
+
 public class NumberComplement {
 	public static void main(String[] args) {
-		System.out.println(new NumberComplement().inverse(5));
+		System.out.println(new NumberComplement().inverse(0));
 	}
 	public int inverse(int a) {
-		int ones = (Integer.highestOneBit(a) << 1) - 1;       // highestOneBit返回最左边的1和后面都是0
+		if (a == 0) {
+	        	return 1;
+	    	}
+	    	if (a == 1) {
+	        	return 0;
+	    	}
+		int ones = (Integer.highestOneBit(a) << 1) - 1;		// highestOneBit返回最左边的1和后边都是0
 		return a ^ ones;
 	} 
 }
-
 
