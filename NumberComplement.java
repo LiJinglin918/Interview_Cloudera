25ee6fefb39840a0fa5bc78f0c5cb8041976e10c
LiJinglin918 <lijinglin918@gmail.com>
2016-11-19 01:47:34 +0700

    Create NumberComplement.java

---
+++ NumberComplement.java
@@ -0,0 +1,15 @@
+/*
+Number Complement。给一个数，输出它的有效数位inverse之后的数（输入输出均为十进制）。
+比如5（101）的inverse是2（010）。因为牵扯到位运算，花了点时间。建议先复习一下位运算（与、或、非、异或）
+*/
+public class NumberComplement {
+	public static void main(String[] args) {
+		System.out.println(new NumberComplement().inverse(5));
+	}
+	public int inverse(int a) {
+		int ones = (Integer.highestOneBit(a) << 1) - 1;       // highestOneBit返回最左边的1和后面都是0
+		return a ^ ones;
+	} 
+}
+
+
